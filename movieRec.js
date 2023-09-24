870f1c6e3414f09faa823d4d8a10b416838f3c85
Sher213 <alisher213@outlook.com>
2023-09-24 14:41:36 -0400

    added html and css renamed query.json to moviePage.json

--- movieRec.js
+++ movieRec.js
@@ -1,3 +1,8 @@
-//open query.json and print related movies
-const movieQuery = require('./query.json');
-console.log(movieQuery["knowledge_graph"]["people_also_search_for"]);
+//open moviePage.json and rcmmndPage.json
+const movieJSON = require('./moviePage.json');
+const rcmmndJSON = require('./rcmmndPage.json');
+
+//extract movieKnowledgeGraph
+movieKnowledge = movieJSON["knowledge_graph"];
+
+//console.log(movieQuery["knowledge_graph"]["people_also_search_for_link"]);
--- movieRec.py
+++ movieRec.py
@@ -2,20 +2,28 @@ from serpapi import GoogleSearch
 import subprocess
 import config
 import json
-#set search
-search = GoogleSearch({
+
+#set searches
+movieSearch = GoogleSearch({
     "q": "Inglorious Basterds",
     "api_key": config.api_key
   })
 
-#get JSON result
-result = search.get_json()
-json_obj = json.dumps(result, indent=4)
+rcmmndSearch = GoogleSearch ({
+    "q": "Inglorious Basterds" + "related movies",
+    "api_key": config.api_key
+})
 
-#and save as query.json file
-with open("query.json", "w") as outfile:
-    outfile.write(json_obj)
+#get JSON result
+movieResult = movieSearch.get_json()
+rcmmndResult = rcmmndSearch.get_json()
+
+#write as as movieQPage.json and movieRPage.json files
+with open("moviePage.json", "w") as outfile:
+    json.dump(movieResult, outfile, indent=4)
+with open("rcmmndPage.json", "w") as outfile:
+    json.dump(rcmmndResult, outfile, indent=4)
 
 #open query.json in Javascript
 #print(json_obj)
-subprocess.run("node movieRec.js query.json")
+subprocess.run("node movieRec.js")
